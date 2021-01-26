42307e96ca6788ea72503faead06f7e81fdf1932
Amit <amit840901@gmail.com>
2021-01-27 04:09:31 +0530

    SelectUserProfile Components

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { BrowseContainer } from '../containers/browse'
 import { useContent } from '../hooks'
+import { selectionFilter } from '../utils'
 
 export default function Browse() {
   const { series } = useContent('series')
   const { films } = useContent('films')
 
-  console.log(series)
-  console.log(films)
-  return <h1>Browse Page</h1>
+  const slides = selectionFilter({ series, films })
+
+  return <BrowseContainer slides={slides} />
 }
